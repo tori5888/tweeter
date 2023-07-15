a1ccae18db5c007d9e2ae1ad025d65e088e4c288
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-07-15 22:28:56 +0000

    trying to impliment/test client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,72 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// Fake data taken from initial-tweets.json
 
+$(document).ready(function() {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+    // Function to render tweets
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+    // Function to create tweet HTML structure
+  const createTweetElement = function(tweet) {
+    const $tweet = `
+      <article class="tweet">
+        <header>
+          <div class="user-info">
+            <img class="avatar" src="${tweet.user.avatars}" alt="User Avatar">
+            <div>
+              <h3 class="user-name">${tweet.user.name}</h3>
+              <span class="user-handle">${tweet.user.handle}</span>
+            </div>
+          </div>
+        </header>
+        <div class="tweet-content">
+          <p>${tweet.content.text}</p>
+        </div>
+        <footer>
+          <span class="tweet-timestamp">${timeago.format(tweet.created_at)}</span>
+          <div class="tweet-actions">
+            <i class="fas fa-heart"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-flag"></i>
+          </div>
+        </footer>
+      </article>
+    `;
+    return $tweet;
+  };
+
+
+  renderTweets(data);
+});
