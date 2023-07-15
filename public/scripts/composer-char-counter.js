020bb79f23522043c12c4744ccb558ca66e412ea
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-07-15 18:18:28 +0000

    add link to char count CSS in index

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-  // --- our code goes here ---
+  $(document).ready(function() {
+    $('textarea[name="text"]').on('input', function() {
+      const textLength = $(this).val().length;
+      const counter = $(this).siblings('.counter');
+      const remainingChars = 140 - textLength;
+
+      counter.text(remainingChars);
+
+    });
+  });
+
 });
