8affdfc9ad621f76ee69b99f69ef62fb541cd648
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-07-16 00:48:03 +0000

    add timeago script

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,6 @@
 // Fake data taken from initial-tweets.json
 
 $(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
-  // Function to render tweets
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
@@ -39,7 +13,6 @@ $(document).ready(function() {
     }
   };
 
-  // Function to create tweet HTML structure (should look similar to hardcoded one originally)
   const createTweetElement = function(tweet) {
     const $tweet = $(`
       <article class="tweet">
@@ -57,6 +30,18 @@ $(document).ready(function() {
     return $tweet;
   };
 
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      type: 'GET',
+      success: function(tweets) {
+        renderTweets(tweets);
+      },
+      error: function(error) {
+        console.error(error);
+      }
+    });
+  };
 
   $('form').submit(function(event) {
     event.preventDefault();
@@ -71,6 +56,8 @@ $(document).ready(function() {
       data: formData,
       success: function(response) {
         console.log(response);
+        // After successful submission, reload the tweets
+        loadTweets();
       },
       error: function(error) {
         console.error(error);
@@ -78,9 +65,6 @@ $(document).ready(function() {
     });
   });
 
-
-
-  renderTweets(data);
+  // Load the tweets on initial page load
+  loadTweets();
 });
-
-
