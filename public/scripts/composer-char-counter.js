e0a4f48d48f7be48cd1a837ed045ab9f6316f6bf
Victoria Kalyniuk <tekalyniuk@hotmail.com>
2023-07-18 21:09:59 +0000

    form validaton function start

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,12 +6,31 @@ $(document).ready(function() {
     const textLength = $(this).val().length;
     const counter = $(this).siblings('.counter');
     const remainingChars = MAX_CHARACTERS - textLength;
+
     counter.text(remainingChars);
 
     if (remainingChars < 0) {
       counter.addClass('redText');
     } else {
       counter.removeClass('redText');
+    }
+  });
+
+  $('.new-tweet form').on('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting
+
+    const tweetTextarea = $('.new-tweet textarea[name="text"]');
+    const tweetContent = tweetTextarea.val().trim(); // Trim whitespace
+
+    if (tweetContent === '') {
+      alert('Error: Your tweet cannot be empty.');
+    } else if (tweetContent.length > MAX_CHARACTERS) {
+      alert('Error: Your tweet exceeds the maximum character limit.');
+    } else {
+      // If validation passes, you can proceed with sending the form data to the server.
+      // For now, we'll just log a success message and clear the textarea.
+      console.log('Tweet sent successfully:', tweetContent);
+      tweetTextarea.val(''); // Clear the textarea
     }
   });
 });
